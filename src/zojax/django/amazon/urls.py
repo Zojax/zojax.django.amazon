202e55788cc424003c3fc000fdb85a3228b7d7fe
cornelius <cornelius@65ff189f-35ee-0310-953c-23049635c3cf>
2010-05-11 12:41:54 +0000

    wefollow:   1. limit tweeters list to 50 items   2. sort tweeters by followers number   3. hide frontpage views if they do not contain any content   4. bugfixes

--- src/zojax/django/amazon/urls.py
+++ src/zojax/django/amazon/urls.py
@@ -3,6 +3,6 @@ import dselector
 parser = dselector.Parser()
 
 urlpatterns = parser.patterns('zojax.django.amazon.views',
-    (r'books/{id:digits}-{slug:slug}', 'view_book', {}, "view_book")
+    (r'books/{id:digits}-{slug:chunk}', 'view_book', {}, "view_book")
 )
 
